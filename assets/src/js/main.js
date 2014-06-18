f1ce143a0051cdcb692a1810360d9cbc9f728122
Phil <phil@demeisen.default>
2014-06-18 22:30:51 +0100

    Removing setHeroImageHeight() function

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,60 +1,49 @@
-function addClass(ele, class){
+function addClass(ele, classname){
 	'use strict';
-	var className = ele.className;
-	var classMatch = new RegExp('\\s?' + class + '\\s?');
+	var className = ele.className,
+		classMatch = new RegExp('\\s?' + classname + '\\s?');
 	if( !className.match(classMatch) ){
-		ele.className += ' ' + class;
+		ele.className += ' ' + classname;
 	}
 }
 
-function removeClass(ele, class){
+function removeClass(ele, classname){
 	'use strict';
-	var classMatch = new RegExp('\\s?' + class);
+	var classMatch = new RegExp('\\s?' + classname);
 	ele.className = ele.className.replace(classMatch, '');
-}
-
-function setHeroImageHeight(){
-	'use strict';
-	var mastheight = document.querySelector('.mast').scrollHeight;
-	var intro = document.querySelector('.intro');
-	if (intro){
-		var winheight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-		var resize = winheight - mastheight;
-		document.querySelector('.intro').style.height = resize + 'px';
-	}
 }
 
 function handleFixedNav(){
 	'use strict';
 	
-	var docheight 	= document.body.scrollHeight;
-	var winheight 	= window.screen.height;
-	var mast 		= document.querySelector('.mast');
-	var body 		= document.body;
-	var bgp 		= 0;
+	var docheight   = document.body.scrollHeight,
+		winheight   = window.screen.height,
+		mast        = document.querySelector('.mast'),
+		body        = document.body,
+		bgp         = 0;
 
 	window.onscroll = function(e){
 		var doc = document.body;
 		
 		if(document.body.scrollWidth > 700){
 			if(doc.scrollTop > 40){
-		    	addClass(body, 'off-top');
-		    } else {
-		    	removeClass(body, 'off-top');
-		    }
-		    var range = 2 + ((doc.scrollTop-30)/100);
-	    	if(range > 6){
-	    		range = 6;
-	    	}
-	    	bgp = -doc.scrollTop / range;
-		}
+				addClass(body, 'off-top');
+			} else {
+				removeClass(body, 'off-top');
+			}
+			var range = 2 + ((doc.scrollTop-30)/100);
+			if(range > 6){
+				range = 6;
+			}
+			bgp = -doc.scrollTop / range;
+		};
 
-	 	mast.style.backgroundPositionY = Math.round(bgp) + 'px';	
-	    body.style.backgroundPositionY = Math.round(bgp) + 'px';	
-	}	
+		mast.style.backgroundPositionY = Math.round(bgp) + 'px';
+		body.style.backgroundPositionY = Math.round(bgp) + 'px';
+	}
 }
 
 (function(){
+	'use strict';
 	handleFixedNav();
-	setHeroImageHeight();
-})();
+}());
