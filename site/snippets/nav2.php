2cdbfe1b273ccedb06467a188ef05b400afe6ee8
Phil <phil@demeisen.default>
2014-06-21 02:37:01 +0100

    Restyling nav2 so it works on small devices

--- site/snippets/nav2.php
+++ site/snippets/nav2.php
@@ -6,17 +6,18 @@ $items = ($open) ? $open->children()->visible()->flip() : false;
 
 ?>
 <?php if($items && $items->count()): ?>
-	<nav class="nav2">
-		<?php if($page->parent()): ?>
-			
-		<?php endif ?>
-		<h2>
-			<a href="<?= $page->parent()->url() ?>">
-				<?= $page->parent()->title() ?>
-			</a>
-		</h2>
-	    <?php foreach($items AS $item): ?>
-	    	<a <?= ($item->isOpen()) ? ' class="active"' : '' ?> href="<?= $item->url() ?>"><?= html($item->title()) ?></a>
-	    <?php endforeach ?>            
-	</nav>
+	<section class="nav2">
+		<div class="row">
+			<h2>
+				<a href="<?= $page->parent()->url() ?>">
+					<?= $page->parent()->title() ?>
+				</a>
+			</h2>
+			<nav>
+			    <?php foreach($items AS $item): ?>
+			    	<a <?= ($item->isOpen()) ? ' class="active"' : '' ?> href="<?= $item->url() ?>"><?= html($item->title()) ?></a>
+			    <?php endforeach ?>            
+			</nav>
+		</div>
+	</section>
 <?php endif ?>
