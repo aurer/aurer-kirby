0242ab6dc4f0867f6e780d52c3a9e8585b23a755
Phil <phil@demeisen.default>
2014-06-24 18:53:15 +0100

    Updating text

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -10,7 +10,7 @@
         <section class="main">
             <div class="row">
                 <div class="content">
-                    <h1><?php html($page->title()) ?></h1>
+                    <h1><?= html($page->title()) ?></h1>
                     <?= kirbytext($page->text()) ?>
                     <?= snippet('projects') ?>
                 </div>
