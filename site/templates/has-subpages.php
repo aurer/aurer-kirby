017c3ef902639d39824a9f3c93e16806d3a7552b
Phil Maurer <philmau@gmail.com>
2015-01-11 17:15:15 +0000

    Updating has-subpages template to use more generic class names

--- site/templates/has-subpages.php
+++ site/templates/has-subpages.php
@@ -6,9 +6,9 @@
                     <h1><?= html($page->title()) ?></h1>
                     <?= kirbytext($page->text()) ?>
 					
-					<nav class="post-list">
+					<nav class="subpages">
 					  	<?php foreach($page->children()->sortBy('date', 'desc') AS $p): ?>
-                            <div class="post">
+                            <div class="subpage">
                                 <h2><a <?= ($p->isOpen()) ? ' class="active"' : '' ?> href="<?= $p->url() ?>"><?= html($p->title()) ?></a></h2>
                                 <span class="date"><?= $p->date('jS M Y') ?></span>
                                 <div class="summary"><?= $p->summary() ?></div>
