a4c817727788a3f854ef9fb8e4a241f3b4454018
root <root@packer-virtualbox-iso.vagrantup.com>
2015-03-14 21:48:13 +0000

    Adding an account and folders required by panel

---
+++ site/accounts/phil.php
@@ -0,0 +1,9 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: phil
+email: philmau@gmail.com
+password: >
+  $2a$10$Gb8M4cKIJ/.ie1LGqVw3Qu3eE53tRZcZt9h3pKAzck0rjvtSH4AOy
+language: en
+role: admin
+token: 9572af320ddb0c7544597a708321af0933389449
