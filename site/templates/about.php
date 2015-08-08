cd179c9d7d288d90b7f884608d261ec33960eb70
Phil Maurer <philmau@gmail.com>
2015-08-08 14:23:49 +0100

    Improving about page content

--- site/templates/about.php
+++ site/templates/about.php
@@ -11,7 +11,21 @@
 
 	<div class="section section--experience">
 		<div class="section-inner">
-			<?= kirbytext($page->text_experience()) ?>
+			<h2 class="section-title">Experience</h2>
+			<div class="grid grid--padHorizontal">
+				<div class="col-lg-1of3">
+					<h4>Languages</h4>
+					<?= kirbytext($page->languages()) ?>
+				</div>
+				<div class="col-lg-1of3">
+					<h4>Frameworks etc.</h4>
+					<?= kirbytext($page->frameworks()) ?>
+				</div>
+				<div class="col-lg-1of3">
+					<h4>Applications</h4>
+					<?= kirbytext($page->applications()) ?>
+				</div>
+			</div>
 		</div>
 	</div>
 
