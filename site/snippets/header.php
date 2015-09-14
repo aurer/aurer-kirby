0fd2c829cbd0a3c8f3230d34f34fab9da26a6a01
Phil Maurer <philmau@gmail.com>
2015-09-14 22:56:23 +0100

    Adding favicons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,8 +9,11 @@
 <meta name="robots" content="index, follow" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<link rel="apple-touch-icon" href="/assets/gfx/apple-touch-icon.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/favicons/favicon-194x194.png" sizes="194x194">
 
 <?= css(asset_path('assets/css', 'screen.css')) ?>
 
