c19bad0aa31ccb505503832cc0608f37ca3ff797
Phil Maurer <philmau@gmail.com>
2015-10-11 23:13:58 +0100

    Tweaking template classes

--- site/templates/about.php
+++ site/templates/about.php
@@ -3,6 +3,10 @@
 	<div class="section section--main">
 		<div class="section-inner">
 			<h1><?= html($page->title()) ?></h1>
+		</div>
+	</div>
+	<div class="section section--primary">
+		<div class="section-inner">
 			<?= kirbytext($page->text()) ?>
 		</div>
 	</div>
